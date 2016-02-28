3e78e39555f630ecb051440c96f715a4aad03812
tasveerj <tasveerj22@gmail.com>
2016-02-28 15:08:43 +0200

    Part two

--- Tut 1/Source.cpp
+++ Tut 1/Source.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 
-	for (int i = 1; i <= 20; i++)
+	for (int i = 1; i <= 50; i++)
 	{
 
 		if (i % 3 != 0 && i % 5 != 0)
@@ -26,4 +26,27 @@ int main()
 		{
 			cout << "FizzBuzz" << endl;
 		}
-	
+
+		else if (i % 7 == 0)
+		{
+			cout << "Woof" << endl;
+		}
+
+		else if (i % 3 == 0 && i % 7 == 0)
+		{
+			cout << "FizzWoof" << endl;
+		}
+
+		else if (i % 5 == 0 && i % 7 == 0)
+		{
+			cout << "BuzzWoof" << endl;
+		}
+
+		else if (i % 3 == 0 && i % 5 == 0 && i & 7 == 0)
+		{
+			cout << "FizzBuzzWoof" << endl;
+		}
+
+
+	}
+	}
